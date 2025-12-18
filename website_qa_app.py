12de88df2391b1db28dd4bf5f42295cb13528132
a-anuj <anujamirthalingam5@gmail.com>
2025-12-18 10:36:47 +0530

    update the imports

--- website_qa_app.py
+++ website_qa_app.py
@@ -5,11 +5,11 @@
 import os
 from groq import Groq
 from langchain_community.document_loaders import WebBaseLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.embeddings import HuggingFaceBgeEmbeddings
-from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
 from langchain_core.prompts import ChatPromptTemplate
-from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains import create_retrieval_chain
 import streamlit as st
 from dotenv import load_dotenv
